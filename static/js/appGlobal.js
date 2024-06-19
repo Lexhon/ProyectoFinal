4aa7db701da7b0db896b2ba80291b2ccc80986c7
EC2 Default User <ec2-user@ip-172-31-55-226.ec2.internal>
2024-06-19 19:57:09 +0000

    version1

---
+++ static/js/appGlobal.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado. ¡Gracias por registrarte o consultar con nosotros!');
+});
